f11b244fcba0afcd5b9119a028e2498b5adfa41a
Manasviganesan <manasviganesan@gmail.com>
2025-02-26 21:23:26 +0530

    Removed api keys from temp.js

--- frontend/temp.js
+++ frontend/temp.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDe6y6m73axNAjBfMxaY-BiP9vy0Qtt_so",
-    authDomain: "smart-parking-applicatio-566ed.firebaseapp.com",
-    projectId: "smart-parking-applicatio-566ed",
-    storageBucket: "smart-parking-applicatio-566ed.firebasestorage.app",
-    messagingSenderId: "924586559087",
-    appId: "1:924586559087:web:86cc158807866fe49e2f9a",
-    measurementId: "G-PZ7KT7F4RC"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 
