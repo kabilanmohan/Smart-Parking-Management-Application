16248e1ea696707279c503f8d2e6cf8f2f9ea77a
Kabilan M D <130450689+kabilanmohan@users.noreply.github.com>
2025-02-27 10:43:06 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+};
 
 // Initialize Firebase app
 const app = initializeApp(firebaseConfig);
